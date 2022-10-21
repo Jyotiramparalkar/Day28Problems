03b05a42970872f1779471fc88d4e103a99ba243
Jyotiramparalkar <paralkarjyotiram@gmail.com>
2022-10-21 22:06:54 +0530

    Start the address book program with CSV

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBookOperations addressBookOperations = new AddressBookOperations();
+        addressBookOperations.performOperationsOnAddressBook();
+    }
+}
